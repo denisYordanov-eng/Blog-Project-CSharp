9b4f2876608af4041b768495b4039e076e4bd252
denisYordanov-eng <denisivov437@gmail.com>
2025-09-20 13:56:36 +0300

    Initial commit.

---
+++ Blog-CSharp-Project/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Blog_CSharp_Project.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Blog_CSharp_Project.Models.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Blog_CSharp_Project.Models.BlogDbContext";
+        }
+
+        protected override void Seed(Blog_CSharp_Project.Models.BlogDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Blog-CSharp-Project/Startup.cs
@@ -0,0 +1,18 @@
+﻿//using Blog_CSharp_Project.Migrations;
+//using Blog_CSharp_Project.Models;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(Blog_CSharp_Project.Startup))]
+namespace Blog_CSharp_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            ConfigureAuth(app);
+        }
+    }
+}
